2977a55a40849af40e19c3b4a4b05528a12ee5ca
Clemens Fehr <clemens.fehr@sap.com>
2021-10-26 17:42:40 +0200

    project setup

---
+++ ShadedPathV/ShadedPathVLib/ShadedPathVLib.cpp
@@ -0,0 +1,10 @@
+// ShadedPathVLib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnShadedPathVLib()
+{
+}
