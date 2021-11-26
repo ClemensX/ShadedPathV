9c6bffb2a0a12e392d5cba749914df4f98f799bb
Clemens Fehr <clemens.fehr@sap.com>
2021-11-26 15:11:14 +0100

    VK_GOOGLE_DISPLAY_TIMING_EXTENSION not available

--- ShadedPathV/ShadedPathVLib/GlobalRendering.cpp
+++ ShadedPathV/ShadedPathVLib/GlobalRendering.cpp
@@ -162,6 +162,7 @@ std::vector<const char*> GlobalRendering::getRequiredExtensions() {
         extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
     }
     extensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
+    //extensions.push_back(VK_GOOGLE_DISPLAY_TIMING_EXTENSION_NAME);
     Log("requested Vulkan extensions:" << endl)
         Util::printCStringList(extensions);
 
