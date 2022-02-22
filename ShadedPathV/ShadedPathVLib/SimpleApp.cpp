0b56feeff230d97fc7340d4a145a20c47307d154
Clemens Fehr <clemens.fehr@sap.com>
2022-02-22 13:44:02 +0100

    camera tryout

--- ShadedPathV/ShadedPathVLib/SimpleApp.cpp
+++ ShadedPathV/ShadedPathVLib/SimpleApp.cpp
@@ -9,6 +9,7 @@ void SimpleApp::run()
         CameraPositioner_FirstPerson positioner(glm::vec3(0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         Camera camera(positioner);
         this->camera = &camera;
+        this->positioner = &positioner;
         engine.enableKeyEvents();
         engine.enableMousButtonEvents();
         engine.enableMouseMoveEvents();
@@ -59,10 +60,14 @@ void SimpleApp::updatePerFrame(ThreadResources& tr)
     double seconds = engine.gameTime.getTimeSeconds();
     if (old_seconds > 0.0f && old_seconds == seconds) {
         Log("DOUBLE TIME" << endl);
+        return;
     }
     if (old_seconds > seconds) {
         Log("INVERTED TIME" << endl);
+        return;
     }
+    double deltaSeconds = seconds - old_seconds;
+    positioner->update(deltaSeconds, input.pos, input.pressedLeft);
     old_seconds = seconds;
     SimpleShader::UniformBufferObject ubo{};
     static bool downmode;
@@ -77,7 +82,8 @@ void SimpleApp::updatePerFrame(ThreadResources& tr)
     //Log(" " << cam << " " << downmode <<  " " << rel_time << endl);
     float cpos = 14.3f;
     glm::vec3 camPos(cpos, cpos, cpos);
-    glm::vec3 objectPosV(objectPos, objectPos, objectPos);
+    //glm::vec3 objectPosV(objectPos, objectPos, objectPos);
+    glm::vec3 objectPosV(0.0f, 0.0f, 0.0f);
 
     glm::mat4 rot = glm::rotate(glm::mat4(1.0f), (float)((seconds * 1.0f) * glm::radians(90.0f)), glm::vec3(0.0f, 0.0f, 1.0f));
     glm::mat4 trans = glm::translate(glm::mat4(1.0f), objectPosV);
@@ -89,19 +95,45 @@ void SimpleApp::updatePerFrame(ThreadResources& tr)
     // flip y:
     ubo.proj[1][1] *= -1;
 
+    ubo.view = camera->getViewMatrix();
+
     // copy ubo to GPU:
     engine.shaders.simpleShader.uploadToGPU(tr, ubo);
 }
 
 void SimpleApp::handleInput(InputState& inputState)
 {
-    if (inputState.keyEvent) {
-        Log("key pressed: " << inputState.key << endl);
-    }
     if (inputState.mouseButtonEvent) {
-        Log("mouse button pressed (left/right): " << inputState.pressedLeft << " / " << inputState.pressedRight << endl);
+        //Log("mouse button pressed (left/right): " << inputState.pressedLeft << " / " << inputState.pressedRight << endl);
+        input.pressedLeft = inputState.pressedLeft;
+        input.pressedRight = inputState.pressedRight;
     }
     if (inputState.mouseMoveEvent) {
-        Log("mouse pos (x/y): " << inputState.pos.x << " / " << inputState.pos.y << endl);
+        //Log("mouse pos (x/y): " << inputState.pos.x << " / " << inputState.pos.y << endl);
+        input.pos.x = inputState.pos.x;
+        input.pos.y = inputState.pos.y;
+    }
+    if (inputState.keyEvent) {
+        //Log("key pressed: " << inputState.key << endl);
+        auto key = inputState.key;
+        auto action = inputState.action;
+        auto mods = inputState.mods;
+        const bool press = action != GLFW_RELEASE;
+        if (key == GLFW_KEY_W)
+            positioner->movement.forward_ = press;
+        if (key == GLFW_KEY_S)
+            positioner->movement.backward_ = press;
+        if (key == GLFW_KEY_A)
+            positioner->movement.left_ = press;
+        if (key == GLFW_KEY_D)
+            positioner->movement.right_ = press;
+        if (key == GLFW_KEY_1)
+            positioner->movement.up_ = press;
+        if (key == GLFW_KEY_2)
+            positioner->movement.down_ = press;
+        if (mods & GLFW_MOD_SHIFT)
+            positioner->movement.fastSpeed_ = press;
+        if (key == GLFW_KEY_SPACE)
+            positioner->setUpVector(glm::vec3(0.0f, 1.0f, 0.0f));
     }
 }
--- ShadedPathV/ShadedPathVLib/SimpleApp.h
+++ ShadedPathV/ShadedPathVLib/SimpleApp.h
@@ -9,5 +9,10 @@ private:
     ShadedPathEngine engine;
     void updatePerFrame(ThreadResources& tr);
     Camera* camera;
+    CameraPositioner_FirstPerson* positioner;
+    // mouse pos in device coords: [0..1]
+    //glm::vec2 mouseDevicePos = glm::vec2(0.0f);
+    InputState input;
+
 };
 
