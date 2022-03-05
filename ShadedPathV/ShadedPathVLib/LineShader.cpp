35dc4fa7b42e35b0728d8953333c24b7676b2ef0
Clemens Fehr <clemensfehr@gmx.de>
2022-03-05 11:50:55 +0100

    Lines ok

--- ShadedPathV/ShadedPathVLib/LineShader.cpp
+++ ShadedPathV/ShadedPathVLib/LineShader.cpp
@@ -90,8 +90,8 @@ void LineShader::initSingle(ThreadResources& tr)
 	rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
 	rasterizer.depthClampEnable = VK_FALSE;
 	rasterizer.rasterizerDiscardEnable = VK_FALSE;
-	rasterizer.polygonMode = VK_POLYGON_MODE_LINE;
-	rasterizer.lineWidth = 10.0f;
+	rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+	rasterizer.lineWidth = 1.0f;
 	rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
 	//rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
 	rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
--- ShadedPathV/ShadedPathVLib/ThreadResources.cpp
+++ ShadedPathV/ShadedPathVLib/ThreadResources.cpp
@@ -69,7 +69,7 @@ void ThreadResources::createRenderPassSimpleShader()
     depthAttachment.format = engine->global.depthFormat;
     depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
     depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
     depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     depthAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
     depthAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
