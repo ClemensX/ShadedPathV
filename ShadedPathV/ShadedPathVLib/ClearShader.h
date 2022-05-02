ae5a0fd73e94b2fa8ebd7c563ae6809f9c10996b
Clemens Fehr <clemens.fehr@sap.com>
2022-05-02 14:36:27 +0200

    cleanup

--- ShadedPathV/ShadedPathVLib/ClearShader.h
+++ ShadedPathV/ShadedPathVLib/ClearShader.h
@@ -6,73 +6,6 @@
 class ClearShader : public ShaderBase
 {
 public:
-    struct Vertex {
-        glm::vec3 pos;
-        glm::vec3 color;
-        glm::vec2 texCoord;
-    };
-
-    const vector<Vertex> vertices = {
-        //{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-        //{{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
-        //{{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-        //{{-0.5f, 0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}},
-
-        {{-0.5f, -0.5f, -10.1f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-        {{0.5f, -0.5f, -10.1f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
-        {{0.5f, 0.5f, -10.1f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-        {{-0.5f, 0.5f, -10.1f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}},
-
-
-        { {-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-        {{0.5f, -0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
-        {{0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-        {{-0.5f, 0.5f, -0.5f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}}
-    };
-
-    const vector<uint16_t> indices = {
-    0, 1, 2, 2, 3, 0,
-    4, 5, 6, 6, 7, 4
-
-    };
-
-    struct UniformBufferObject {
-        glm::mat4 model;
-        glm::mat4 view;
-        glm::mat4 proj;
-    };
-
-    static VkVertexInputBindingDescription getBindingDescription() {
-        VkVertexInputBindingDescription bindingDescription{};
-        bindingDescription.binding = 0;
-        bindingDescription.stride = sizeof(Vertex);
-        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-        return bindingDescription;
-    }
-
-    // get static std::array of attribute desciptions, make sure to copy to local array, otherwise you get dangling pointers!
-    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions() {
-        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
-        // layout(location = 0) in vec3 inPosition;
-        attributeDescriptions[0].binding = 0;
-        attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(Vertex, pos);
-        // layout(location = 1) in vec3 inColor;
-        attributeDescriptions[1].binding = 0;
-        attributeDescriptions[1].location = 1;
-        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[1].offset = offsetof(Vertex, color);
-        // layout(location = 2) in vec2 inTexCoord;
-        attributeDescriptions[2].binding = 0;
-        attributeDescriptions[2].location = 2;
-        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
-
-        return attributeDescriptions;
-    }
-
-
     // set up shader
     virtual void init(ShadedPathEngine& engine, ShaderState &shaderState) override;
     virtual void initSingle(ThreadResources& tr, ShaderState& shaderState) override;
@@ -81,22 +14,11 @@ public:
 
     virtual ~ClearShader() override;
 
-    // pre-record draw commands (one time call)
-    void recordDrawCommand(VkCommandBuffer& commandBuffer, ThreadResources& tr, VkBuffer vertexBuffer, VkBuffer indexBuffer);
-    
     // create descriptor set layout (one per effect)
     virtual void createDescriptorSetLayout() override;
     // create descritor sets (one or more per render thread)
     virtual void createDescriptorSets(ThreadResources& res) override;
 
-    TextureInfo* texture = nullptr;
-
 private:
-    VkShaderModule vertShaderModuleTriangle = nullptr;
-    VkShaderModule fragShaderModuleTriangle = nullptr;
-    VkBuffer vertexBufferTriangle = nullptr;
-    VkDeviceMemory vertexBufferMemoryTriangle = nullptr;
-    VkBuffer indexBufferTriangle = nullptr;
-    VkDeviceMemory indexBufferMemoryTriangle = nullptr;
 };
 
