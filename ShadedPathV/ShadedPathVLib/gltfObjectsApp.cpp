83223a076b0e257d8a56e13b9b3e72fb45cfe8b8
Clemens Fehr <clemensfehr@gmx.de>
2022-12-27 07:56:33 +0100

    temp

--- ShadedPathV/ShadedPathVLib/gltfObjectsApp.cpp
+++ ShadedPathV/ShadedPathVLib/gltfObjectsApp.cpp
@@ -26,6 +26,7 @@ void gltfObjectsApp::run()
         engine.setBackBufferResolution(ShadedPathEngine::Resolution::FourK);
         //engine.setBackBufferResolution(ShadedPathEngine::Resolution::OneK); // 960
         int win_width = 1800;//480;// 960;//1800;// 800;//3700;
+        //engine.enablePresentation(win_width, (int)(win_width / 3.55f), "Render glTF objects");
         engine.enablePresentation(win_width, (int)(win_width / 1.77f), "Render glTF objects");
         camera.saveProjection(perspective(glm::radians(45.0f), engine.getAspect(), 0.1f, 2000.0f));
 
