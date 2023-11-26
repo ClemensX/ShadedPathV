92c39f2197483dbf1cd4cba23196f0f7787deadd
Clemens Fehr <clemensfehr@gmx.de>
2023-11-26 17:29:29 +0100

    initial app build

--- shadedpath_lib/MakeTable.cmake
+++
@@ -1,8 +0,0 @@
-# custome build via new executable:
-add_executable(MakeTable makeTable.cpp)
-target_link_libraries(MakeTable PRIVATE sp_compiler_flags)
-add_custom_command(
-  OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/Table.h
-  COMMAND MakeTable ${CMAKE_CURRENT_BINARY_DIR}/Table.h
-  DEPENDS MakeTable
-  )
