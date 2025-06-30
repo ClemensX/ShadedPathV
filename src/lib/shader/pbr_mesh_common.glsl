88f0c4ff4316886dd104927dd57a70591618a218
ClemensX <clemensfehr@gmx.de>
2025-06-30 10:03:11 +0200

    util function for object pixel size test

--- src/lib/shader/pbr_mesh_common.glsl
+++ src/lib/shader/pbr_mesh_common.glsl
@@ -118,3 +118,126 @@ layout(binding = 0) uniform UniformBufferObject {
 } ubo;
 
 
+// utility functions
+
+// check if object AABB is completely outside view frustrum
+bool isOutsideView(BoundingBox bb, mat4 mvp) {
+    vec3 aabbMin = bb.min;
+    vec3 aabbMax = bb.max;
+
+    // generate 8 corners of AABB:
+    vec3 corners[8];
+    corners[0] = vec3(aabbMin.x, aabbMin.y, aabbMin.z);
+    corners[1] = vec3(aabbMax.x, aabbMin.y, aabbMin.z);
+    corners[2] = vec3(aabbMin.x, aabbMax.y, aabbMin.z);
+    corners[3] = vec3(aabbMax.x, aabbMax.y, aabbMin.z);
+    corners[4] = vec3(aabbMin.x, aabbMin.y, aabbMax.z);
+    corners[5] = vec3(aabbMax.x, aabbMin.y, aabbMax.z);
+    corners[6] = vec3(aabbMin.x, aabbMax.y, aabbMax.z);
+    corners[7] = vec3(aabbMax.x, aabbMax.y, aabbMax.z);
+
+    // Transform corners to clip space
+    vec4 clipCorners[8];
+    for (int i = 0; i < 8; ++i) {
+        clipCorners[i] = mvp * vec4(corners[i], 1.0);
+    }
+
+    // For each plane, if all corners are outside, the object is outside the frustum:
+    bool outside = false;
+    for (int plane = 0; plane < 6; ++plane) {
+        int outCount = 0;
+        for (int i = 0; i < 8; ++i) {
+            vec4 c = clipCorners[i];
+            if (plane == 0 && c.x < -c.w) outCount++; // left
+            if (plane == 1 && c.x >  c.w) outCount++; // right
+            if (plane == 2 && c.y < -c.w) outCount++; // bottom
+            if (plane == 3 && c.y >  c.w) outCount++; // top
+            if (plane == 4 && c.z <  0.0) outCount++; // near (Vulkan)
+            if (plane == 5 && c.z >  c.w) outCount++; // far
+        }
+        if (outCount == 8) {
+            outside = true;
+            break;
+        }
+    }
+
+    return outside;
+}
+
+// return 1 or 2 if object outside view frustrum or pixel size too small
+int isOutsideViewOrTooSmall(BoundingBox bb, mat4 mvp) {
+    //const vec2 viewParams_screenSize = vec2(4096.0, 4096.0);
+    const vec2 viewParams_screenSize = vec2(1800.0, 1017.0);
+    vec3 aabbMin = bb.min;
+    vec3 aabbMax = bb.max;
+
+    // generate 8 corners of AABB:
+    vec3 corners[8];
+    corners[0] = vec3(aabbMin.x, aabbMin.y, aabbMin.z);
+    corners[1] = vec3(aabbMax.x, aabbMin.y, aabbMin.z);
+    corners[2] = vec3(aabbMin.x, aabbMax.y, aabbMin.z);
+    corners[3] = vec3(aabbMax.x, aabbMax.y, aabbMin.z);
+    corners[4] = vec3(aabbMin.x, aabbMin.y, aabbMax.z);
+    corners[5] = vec3(aabbMax.x, aabbMin.y, aabbMax.z);
+    corners[6] = vec3(aabbMin.x, aabbMax.y, aabbMax.z);
+    corners[7] = vec3(aabbMax.x, aabbMax.y, aabbMax.z);
+
+    vec2 minScreen = vec2(1e10);
+    vec2 maxScreen = vec2(-1e10);
+    // Transform corners to clip space
+    vec4 clipCorners[8];
+    for (int i = 0; i < 8; ++i) {
+        vec4 clip = mvp * vec4(corners[i], 1.0);
+        clipCorners[i] = clip;
+        if (clip.w <= 0.0) continue;
+        vec3 ndc = clip.xyz / clip.w;
+        vec2 screen = (ndc.xy * 0.5 + 0.5) * viewParams_screenSize;
+        minScreen = min(minScreen, screen);
+        maxScreen = max(maxScreen, screen);
+    }
+    float pixelWidth  = maxScreen.x - minScreen.x;
+    float pixelHeight = maxScreen.y - minScreen.y;
+    float maxPixelSize = max(pixelWidth, pixelHeight);
+
+    //debugPrintfEXT("maxPixelSize %f\n", maxPixelSize);
+    if (maxPixelSize < 5.0) {
+        // Cull object
+        return 2;
+    }
+
+    // For each plane, if all corners are outside, the object is outside the frustum:
+    bool outside = false;
+    for (int plane = 0; plane < 6; ++plane) {
+        int outCount = 0;
+        for (int i = 0; i < 8; ++i) {
+            vec4 c = clipCorners[i];
+            if (plane == 0 && c.x < -c.w) outCount++; // left
+            if (plane == 1 && c.x >  c.w) outCount++; // right
+            if (plane == 2 && c.y < -c.w) outCount++; // bottom
+            if (plane == 3 && c.y >  c.w) outCount++; // top
+            if (plane == 4 && c.z <  0.0) outCount++; // near (Vulkan)
+            if (plane == 5 && c.z >  c.w) outCount++; // far
+        }
+        if (outCount == 8) {
+            outside = true;
+            break;
+        }
+    }
+
+    if (outside) return 1;
+    return 0; // visible and big enough
+}
+
+//    int isOutside = isOutsideViewOrTooSmall(model_ubo.boundingBox, mvp);
+//    if (isOutside == 1) {
+//        // Don't emit mesh tasks for this object
+//        BoundingBox bb = model_ubo.boundingBox;
+//        debugPrintfEXT("WARNING: OUTSIDE bb min %f %f %f\n", bb.min.x, bb.min.y, bb.min.z);
+//        return;
+//    } else if (isOutside == 2) {
+//        // Don't emit mesh tasks for this object
+//        BoundingBox bb = model_ubo.boundingBox;
+//        debugPrintfEXT("WARNING: TOO SMALL bb min %f %f %f\n", bb.min.x, bb.min.y, bb.min.z);
+//        return;
+//    }
+//
