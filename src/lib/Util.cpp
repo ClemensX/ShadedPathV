45ea24165a4c706ccc86f1fb9bd5c6a7ec63977e
ClemensX <clemensfehr@gmx.de>
2026-01-19 09:49:37 +0100

    fix broken commit

--- src/lib/Util.cpp
+++ src/lib/Util.cpp
@@ -1115,6 +1115,7 @@ void Util::GenerateCylinderMesh(
             indices.push_back(i2);
         }
     }
+
     // Top cap
     uint32_t topCenterIndex = static_cast<uint32_t>(vertices.size());
     vertices.emplace_back(glm::vec3(0, height / 2.0f, 0), glm::vec3(0, 1, 0), glm::vec2(0.5f, 0.5f));
@@ -1126,6 +1127,41 @@ void Util::GenerateCylinderMesh(
         indices.push_back(topCenterIndex);
         indices.push_back(i0);
         indices.push_back(i1);
+    }
+
+    // Bottom cap
+    uint32_t bottomCenterIndex = static_cast<uint32_t>(vertices.size());
+    vertices.emplace_back(glm::vec3(0, -height / 2.0f, 0), glm::vec3(0, -1, 0), glm::vec2(0.5f, 0.5f));
+    for (int i = 0; i < segments; ++i) {
+        int next = (i + 1) % segments;
+        uint32_t i0 = i;
+        uint32_t i1 = next;
+        indices.push_back(bottomCenterIndex);
+        indices.push_back(i1);
+        indices.push_back(i0);
+    }
+
+    if (produceCrack) {
+        // if we produce a crack, we add more triangles at the top
+        // it0 and it1 are the indices at the top where we left out the first segment
+        uint32_t it0 = heightDivs * segments;
+        uint32_t it1 = heightDivs * segments + 1;
+        // il0 and il1 are one triangle lower than it0 and it1
+        uint32_t il0 = (heightDivs - 1) * segments;
+        uint32_t il1 = (heightDivs - 1) * segments + 1;
+
+        // add triangles to close the gap
+        indices.push_back(topCenterIndex);
+        indices.push_back(it0);
+        indices.push_back(il0);
+
+        indices.push_back(topCenterIndex);
+        indices.push_back(il1);
+        indices.push_back(it1);
+
+        indices.push_back(topCenterIndex);
+        indices.push_back(il0);
+        indices.push_back(il1);
     }
 }
 
