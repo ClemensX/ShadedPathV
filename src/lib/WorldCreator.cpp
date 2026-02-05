ade2e6cab6406031a492c3ef2e59f7b82fe7a08d
ClemensX <clemensfehr@gmx.de>
2026-02-05 13:04:07 +0100

    make WorldCreator imported asset names unique

--- src/lib/WorldCreator.cpp
+++ src/lib/WorldCreator.cpp
@@ -6,6 +6,17 @@ using namespace glm;
 void WorldCreator::load(const std::string& filepath)
 {
     biomeObjects = wcil::WorldCreatorInstanceLoader::LoadInstanceInfo(filepath);
+    // make sure we have unique asset names (not always the case in WC exports)
+    std::set<std::string> existingNames;
+    for (auto& biome : biomeObjects) {
+        std::string originalName = biome.Name;
+        int suffix = 1;
+        while (existingNames.find(biome.Name) != existingNames.end()) {
+            biome.Name = originalName + "_" + std::to_string(suffix);
+            suffix++;
+        }
+        existingNames.insert(biome.Name);
+    }
 }
 
 void WorldCreator::loadBiomeCSVData(const std::string& csvDir, wcil::BiomeObject& biome)
